3b6af35d15bd127711289b9fe820e1841985b0aa
heyitsuzair <uzairdevil354123@gmail.com>
2022-09-29 02:23:07 +0500

    Improvements

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -10,16 +10,22 @@ class CategoryController extends Controller
 {
     function addCategory(Request $req)
     {
-        $category = new Category;
-        $category->name = $req->input('name');
-        // Upload an Image File to Cloudinary with One line of Code
-        $uploadedFileUrl = Cloudinary::upload($req->file('pic')->getRealPath())->getSecurePath();
-        $category->pic = $uploadedFileUrl;
-        $category->save();
-        if ($category) {
-            return response()->json(['error' => false, 'message' => 'Category Added!'], 200);
+        // check if the category with this name already exists, if yes than return a response with error and message
+        $check_exits = Category::where('name', $req->name)->exists();
+        if ($check_exits) {
+            return response()->json(['error' => true, 'message' => 'A Category With This Name Already Exists!'], 400);
         } else {
-            return response()->json(['error' => true, 'message' => 'Internal Server Error!'], 500);
+            $category = new Category;
+            $category->name = $req->input('name');
+            // Upload an Image File to Cloudinary with One line of Code
+            $uploadedFileUrl = $req->file('pic')->storeOnCloudinary('ecommerce-backend-laravel/categories')->getSecurePath();
+            $category->pic = $uploadedFileUrl;
+            $category->save();
+            if ($category) {
+                return response()->json(['error' => false, 'message' => 'Category Added!'], 200);
+            } else {
+                return response()->json(['error' => true, 'message' => 'Internal Server Error!'], 500);
+            }
         }
     }
 }
